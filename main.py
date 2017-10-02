9631843ba040fad398fb7b54cf41730f8503bda2
Kirsty McNaught <kirsty.mcnaught@yahoo.com>
2017-10-02 22:10:49 +0100

    Optionally ignore pronouns

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ VERBIX_URL = "http://api.verbix.com/conjugator/html?language={0}&tableurl=" \
               + VERBIX_TABLE_URL \
               + "&verb={1}"
 
+def element_in_list_of_tuples(the_list, elem):    
+    for item in the_list:
+        if elem in item:
+            return True
+    return False
+
 # Extract any pronouns and conjugated verbs from a verbix
 # conjugation table.
 # Both pronouns and verbs are the only things in "span" elements.
 class ConjugationTableParser(HTMLParser):
     
-    def __init__(self):
+    def __init__(self, ignore_pronouns=False):
         HTMLParser.__init__(self)
         self.relevant_tag = False
         self.data = set([]) 
+        self.ignore_pronouns = ignore_pronouns        
     
     def handle_starttag(self, tag, attrs):
-        if (tag == "span"):
-            self.relevant_tag = True;
+        if (tag == "span"):                     
+            is_pronoun = element_in_list_of_tuples(attrs, 'pronoun');
+            if not is_pronoun or not self.ignore_pronouns:                
+                self.relevant_tag = True;
 
     def handle_endtag(self, tag):
         self.relevant_tag = False;
@@ -27,11 +36,12 @@ class ConjugationTableParser(HTMLParser):
     def handle_data(self, data):
         if self.relevant_tag:
             for word in data.split():
-                self.data.add(word)        
+                self.data.add(word)
+        
 
-def get_conjugations(lang, verb):        
+def get_conjugations(lang, verb, ignore_pronouns):        
     r = requests.get(VERBIX_URL.format(lang, verb))
-    parser = ConjugationTableParser()
+    parser = ConjugationTableParser(ignore_pronouns)
     parser.feed(r.text)
     if len(parser.data) == 0:
 #         print("ERROR: {}".format(r.text))
@@ -42,19 +52,21 @@ def get_conjugations(lang, verb):
 def main():
 
     # Parse command line inputs
-    parser = argparse.ArgumentParser(description='Conjugate a list of verbs')
+    parser = argparse.ArgumentParser(description='Conjugate a list of verbs.')
     parser.add_argument('-l', '--lang', type=str, required=True,
-                        help="3-character language code")
+                        help="3-character language code. For language codes, see http://api.verbix.com/conjugator/html")
     parser.add_argument('-i', '--input', type=str, required=True,
-                       help='input file (list of infinitive verbs)')
+                       help='input file (list of infinitive verbs)')    
     parser.add_argument('-o', '--output', type=str,
                        default="out.txt",
                        help='output file (list of conjugated verbs)')
-
+    parser.add_argument('--ignore-pronouns', action="store_true",
+                        default=False)
     args = parser.parse_args()
     language = args.lang
     input_file = args.input
     output_file = args.output
+    ignore_pronouns = args.ignore_pronouns
     
     # Read input varbs    
     with open(input_file) as f:
@@ -64,7 +76,7 @@ def main():
     all_words = set([])     
     for verb in content:
         print('Conjugating {0}'.format(verb))
-        words = get_conjugations(language, verb)
+        words = get_conjugations(language, verb, ignore_pronouns)
         if words is not None:
             all_words.update(words)
         else:
